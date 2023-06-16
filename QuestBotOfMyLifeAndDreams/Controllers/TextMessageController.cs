b106a9ba65191cfaabdc414d3771b837e01ae6df
Philipp Kucherovskiy <philipp.kucherovskiy@gmail.com>
2023-06-16 03:57:56 +0300

    Добавьте файлы проекта.

---
+++ QuestBotOfMyLifeAndDreams/Controllers/TextMessageController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using QuestBotOfMyLifeAndDreams.Configuration;
+
+namespace QuestBotOfMyLifeAndDreams.Controllers
+{
+    public class TextMessageController
+    {
+        private readonly ITelegramBotClient _telegramClient;
+
+        public TextMessageController(ITelegramBotClient telegramBotClient)
+        {
+            _telegramClient = telegramBotClient;
+        }
+
+        public async Task Handle(Message message, CancellationToken ct)
+        {
+            switch (message.Text)
+            {
+                case "/start":
+                    await _telegramClient.SendTextMessageAsync(message.Chat.Id, $"<b>  Наш бот превращает аудио в текст.</b> {Environment.NewLine}", cancellationToken: ct);
+            break;
+                default:
+                    await _telegramClient.SendTextMessageAsync(message.Chat.Id, "Отправьте '/start' для начала Вашего приключения!  ", cancellationToken: ct);
+                    break;
+
+            }
+        }
+    } }
---
+++ QuestBotOfMyLifeAndDreams/Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using QuestBotOfMyLifeAndDreams;
+using Telegram.Bot;
+using QuestBotOfMyLifeAndDreams.Controllers;
+using QuestBotOfMyLifeAndDreams.Services;
+using QuestBotOfMyLifeAndDreams.Configuration;
+
+namespace QuestBotOfMyLifeAndDreams
+{
+    public class Program
+    {
+        public static async Task Main()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            // Объект, отвечающий за постоянный жизненный цикл приложения
+            var host = new HostBuilder()
+                .ConfigureServices((hostContext, services) => ConfigureServices(services)) // Задаем конфигурацию
+                .UseConsoleLifetime() // Позволяет поддерживать приложение активным в консоли
+                .Build(); // Собираем
+
+            Console.WriteLine("Сервис запущен");
+            // Запускаем сервис
+            await host.RunAsync();
+            Console.WriteLine("Сервис остановлен");
+        }
+
+        static void ConfigureServices(IServiceCollection services)
+        {
+            AppSettings appSettings = BuildAppSettings();
+            services.AddSingleton(BuildAppSettings());
+
+            services.AddSingleton<IStorage, MemoryStorage>();
+
+            services.AddTransient<DefaultMessageController>();
+            services.AddTransient<TextMessageController>();
+            services.AddTransient<InlineController>();
+            services.AddSingleton<IStorage, MemoryStorage>();
+
+            // Регистрируем объект TelegramBotClient c токеном подключения
+            services.AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient("6081836295:AAHbvbEt9_wBHULc7VEmGkXlY4gW5cW7coI"));
+            // Регистрируем постоянно активный сервис бота
+            services.AddHostedService<Bot>();
+        }
+
+        static AppSettings BuildAppSettings()
+        {
+            return new AppSettings()
+            {
+                BotToken = "6081836295:AAHbvbEt9_wBHULc7VEmGkXlY4gW5cW7coI"
+            };
+        }
+    }
+}
