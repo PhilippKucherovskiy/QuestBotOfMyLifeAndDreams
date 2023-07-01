8a75d9d851348bdba5c79a21896de958ee7d52b2
Philipp Kucherovskiy <philipp.kucherovskiy@gmail.com>
2023-07-01 03:26:36 +0300

    Все работает, можно писать игровую логику. Теперь нужно решить, как быть с нажатиями. Можно нажать по 2 раза разные кнопки.

--- QuestBotOfMyLifeAndDreams/Services/GameDictionary.cs
+++ QuestBotOfMyLifeAndDreams/Services/GameDictionary.cs
@@ -10,30 +10,81 @@ namespace QuestBotOfMyLifeAndDreams.Services
         {
             _gameContentLookup = new Dictionary<string, GameContent>();
 
-            // Добавьте начальное содержимое игры в конструкторе
             var startGameContent = new GameContent
             {
                 Text = "Текст 1",
                 Options = new[]
                 {
-                    new Option { Text = "1", NextBlock = "Block1A" },
-                    new Option { Text = "2", NextBlock = "Block2A" }
+                    new Option { Text = "1А", NextBlock = "Block1A" },
+                    new Option { Text = "1В", NextBlock = "Block1B" }
                 }
             };
             AddGameContent("StartBlock", startGameContent);
 
-            // Добавьте другие блоки и их содержимое по необходимости
             var block1AContent = new GameContent
             {
-                Text = "Текст 1А"
+                Text = "Текст 1А",
+                Options = new[]
+                {
+                    new Option { Text = "1Б", NextBlock = "Block1C" },
+                    new Option { Text = "1В", NextBlock = "Block1D" }
+                }
             };
             AddGameContent("Block1A", block1AContent);
 
-            var block2AContent = new GameContent
+            var block1BContent = new GameContent
             {
-                Text = "Текст 2А"
+                Text = "Текст 1Б",
+                Options = new[]
+                {
+                    new Option { Text = "1В", NextBlock = "Block1E" }
+                }
             };
-            AddGameContent("Block2A", block2AContent);
+            AddGameContent("Block1B", block1BContent);
+
+            var block1CContent = new GameContent
+            {
+                Text = "Текст 1С",
+                Options = new[]
+                {
+                    new Option { Text = "1В", NextBlock = "Block1E" }
+                }
+            };
+            AddGameContent("Block1C", block1CContent);
+
+            var block1DContent = new GameContent
+            {
+                Text = "Текст 1D",
+                Options = new[]
+                {
+                    new Option { Text = "14", NextBlock = "Block14" },
+                    new Option { Text = "7", NextBlock = "Block7" }
+                }
+            };
+            AddGameContent("Block1D", block1DContent);
+
+            var block1EContent = new GameContent
+            {
+                Text = "Текст 1E",
+                Options = new[]
+                {
+                    new Option { Text = "14", NextBlock = "Block14" },
+                    new Option { Text = "7", NextBlock = "Block7" }
+                }
+            };
+            AddGameContent("Block1E", block1EContent);
+
+            var block14Content = new GameContent
+            {
+                Text = "Текст 14"
+            };
+            AddGameContent("Block14", block14Content);
+
+            var block7Content = new GameContent
+            {
+                Text = "Текст 7"
+            };
+            AddGameContent("Block7", block7Content);
         }
 
         public void AddGameContent(string blockId, GameContent gameContent)
@@ -55,7 +106,6 @@ namespace QuestBotOfMyLifeAndDreams.Services
     public class GameContent
     {
         public string Text { get; set; }
-        public string ImageUrl { get; set; }
         public Option[] Options { get; set; }
     }
 
