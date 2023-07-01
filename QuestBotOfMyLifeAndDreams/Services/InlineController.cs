9245aeb52160ea4189f8033861ca201f38644d24
Philipp Kucherovskiy <philipp.kucherovskiy@gmail.com>
2023-07-01 03:33:09 +0300

    теперь предыдущие сообщения удаляются

--- QuestBotOfMyLifeAndDreams/Services/InlineController.cs
+++ QuestBotOfMyLifeAndDreams/Services/InlineController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Types.Enums;
@@ -14,6 +13,8 @@ namespace QuestBotOfMyLifeAndDreams.Controllers
     {
         private readonly ITelegramBotClient _telegramClient;
         private readonly GameDictionary _gameDictionary;
+        private long _chatId;
+        private int _previousMessageId;
 
         public InlineController(ITelegramBotClient telegramBotClient, GameDictionary gameDictionary)
         {
@@ -26,21 +27,6 @@ namespace QuestBotOfMyLifeAndDreams.Controllers
             var blockId = callbackQuery.Data;
             var gameContent = _gameDictionary.GetGameContent(blockId);
 
-            if (gameContent != null)
-            {
-                await SendMessage(gameContent, callbackQuery.Message.Chat.Id);
-            }
-        }
-
-        private async Task SendMessage(GameContent gameContent, long chatId)
-        {
-            // Отправить текстовое сообщение
-            await _telegramClient.SendTextMessageAsync(
-                chatId: chatId,
-                text: gameContent.Text
-            );
-
-            // Если есть варианты ответа, отправить клавиатуру с вариантами
             if (gameContent.Options != null && gameContent.Options.Length > 0)
             {
                 var replyMarkup = new InlineKeyboardMarkup(
@@ -49,11 +35,39 @@ namespace QuestBotOfMyLifeAndDreams.Controllers
                     )
                 );
 
-                await _telegramClient.SendTextMessageAsync(
-                    chatId: chatId,
-                    text: "Choose an option:",
+                // Удаляем предыдущее сообщение
+                await DeletePreviousMessage();
+
+                var message = await _telegramClient.SendTextMessageAsync(
+                    chatId: callbackQuery.Message.Chat.Id,
+                    text: gameContent.Text,
                     replyMarkup: replyMarkup
                 );
+
+                _chatId = message.Chat.Id;
+                _previousMessageId = message.MessageId;
+            }
+            else
+            {
+                // Удаляем предыдущее сообщение
+                await DeletePreviousMessage();
+
+                var message = await _telegramClient.SendTextMessageAsync(
+                    chatId: callbackQuery.Message.Chat.Id,
+                    text: gameContent.Text
+                );
+
+                _chatId = message.Chat.Id;
+                _previousMessageId = message.MessageId;
+            }
+        }
+
+        private async Task DeletePreviousMessage()
+        {
+            if (_previousMessageId != 0)
+            {
+                await _telegramClient.DeleteMessageAsync(_chatId, _previousMessageId);
+                _previousMessageId = 0;
             }
         }
     }
